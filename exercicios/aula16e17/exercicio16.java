7909c574f050cd354a5668b564619944241b0b72
V-Renan <victorrenan2018@gmail.com>
2023-07-10 16:04:41 -0300

    new
    exercício

---
+++ exercicios/aula16e17/exercicio16.java
@@ -0,0 +1,31 @@
+package com.victor.cursojava.exercicios.aula16e17;
+
+import java.util.Scanner;
+
+public class exercicio16 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("n-ésimo termo: ");
+        int n = scan.nextInt();
+
+        int primeiro = 1;
+        int segundo = 1;
+        int proximo;
+
+        System.out.print(primeiro + " ");
+        System.out.print(segundo + " ");
+
+        for (int i = 3; i <= n; i++){
+
+            proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+
+            System.out.print(proximo + " ");
+        }
+
+    }
+}
