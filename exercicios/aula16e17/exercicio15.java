11aac123679f8b75be6cd2377a543f8fa37c282f
V-Renan <victorrenan2018@gmail.com>
2023-07-10 15:44:38 -0300

    att

--- exercicios/aula16e17/exercicio15.java
+++ exercicios/aula16e17/exercicio15.java
@@ -7,7 +7,25 @@ public class exercicio15 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        
+
+        System.out.print("n-ésimo termo: ");
+        int n = scan.nextInt();
+
+        int primeiro = 1;
+        int segundo = 1;
+        int proximo;
+
+        System.out.print(primeiro + " ");
+        System.out.print(segundo + " ");
+
+        for (int i = 3; i <= n; i++){
+
+            proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+
+            System.out.print(proximo + " ");
+        }
 
     }
 }
