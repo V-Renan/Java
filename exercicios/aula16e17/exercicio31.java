21fbaeb54a05446d5b6a038e48d6e1823a50494e
V-Renan <victorrenan2018@gmail.com>
2023-07-29 22:06:25 -0300

    up

--- exercicios/aula16e17/exercicio31.java
+++ exercicios/aula16e17/exercicio31.java
@@ -8,6 +8,66 @@ public class exercicio31 {
 
         Scanner scan = new Scanner(System.in);
 
+        double cachorroQuente = 1.20;
+        double bauruSimples = 1.30;
+        double bauruComOvo = 1.50;
+        double hamburguer = 1.20;
+        double cheeseburguer = 1.30;
+        double refri = 1.00;
+        double soma = 0;
+        double qtd;
+        double pedido;
+        double preço = 0;
 
+        System.out.println("                       CARDÁPIO");
+        System.out.println("--------------------------------------------------------");
+
+        System.out.println("Especificação       |       Código       |        Preço ");
+        System.out.println("Cachorro Quente     |        100         |       R$ 1,20");
+        System.out.println("Bauru Simples       |        101         |       R$ 1,30");
+        System.out.println("Bauru com Ovo       |        102         |       R$ 1,50");
+        System.out.println("Hambúrguer          |        103         |       R$ 1,20");
+        System.out.println("Cheeseburguer       |        104         |       R$ 1,30");
+        System.out.println("Refrigerante        |        105         |       R$ 1,00");
+
+        while (true) {
+            System.out.print("Digite o código do seu pedido: ");
+            pedido = scan.nextInt();
+            if (pedido == 100){
+                preço = cachorroQuente;
+            } else if (pedido == 101){
+                preço = bauruSimples;
+            } else if (pedido == 102){
+                preço = bauruComOvo;
+            } else if (pedido == 103){
+                preço = hamburguer;
+            } else if (pedido == 104){
+                preço = cheeseburguer;
+            } else if (pedido == 105){
+                preço = refri;
+            } else {
+                System.out.println("Pedido inválido, Tente novamente!");
+                continue;
+            }
+            while (true) {
+                System.out.print("Digite a quantidade: ");
+                qtd = scan.nextInt();
+                if (qtd > 0){
+                    break;
+                } else {
+                    System.out.println("Quantidade inválida. Tente novamente!");
+                }
+            }
+
+            soma += qtd * preço;
+
+            System.out.print("Deseja adicionar mais algum item? [S/N]");
+            String op = scan.next();
+            if (op.equalsIgnoreCase("N")){
+                break;
+            }
+        }
+
+        System.out.println("Total a ser pago: R$" + soma);
     }
 }
